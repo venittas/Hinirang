4cd1c1186268980d533a4458bf42d70fa06a026e
DaddyJiAr <santiagojr0212@gmail.com>
2026-04-10 11:14:28 +0800

    added simple pause ui with logic

---
+++ Assets/Hinirang/Scripts/PauseManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject PauseUI;
+
+    public void ResumeGame()
+    {
+        Destroy(PauseUI);
+        Time.timeScale = 1f;
+    }
+    public void PauseGame()
+    {
+        Instantiate(PauseUI);
+        Time.timeScale = 0f;
+    }
+
+    public void ToMainMenu()
+    {
+        SceneSystem.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+}
